a3539e4ce73b05bc889763dcba40c1fa8a84dd0b
Fox <thornhillfox@gmail.com>
2020-08-21 18:58:23 +0300

    change reverse

--- Versaproject/views.py
+++ Versaproject/views.py
@@ -2,5 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+def home(request):
+	return render(request, 'home.html')
+
 def reverse(request):
-	return render(request, 'reverse.html')
+	user_text = request.GET['usertext']
+	reverse_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_text})
+
